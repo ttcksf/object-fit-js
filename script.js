38839395fa367d066e3e850dd3ee97e1dbef79be
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-09-18 01:00:39 +0900

    first

---
+++ script.js
@@ -0,0 +1,33 @@
+const video = document.getElementById("myvideo");
+const canvas = document.getElementById("mycanvas");
+
+let model;
+
+const options = {
+  flipHorizontal: false,
+  maxNumBoxes: 3,
+  scoreThreshold: 0.7,
+};
+
+let context = canvas.getContext("2d");
+
+handTrack.load(options).then(function (modelData) {
+  model = modelData;
+  console.log(model);
+
+  //web camera
+  handTrack.startVideo(video).then(function (status) {
+    if (status) {
+      startDetection();
+    } else {
+      console.log("failed");
+    }
+  });
+});
+
+function startDetection() {
+  model.detect(video).then((predictions) => {
+    model.renderPredictions(predictions, canvas, context, video);
+    requestAnimationFrame(startDetection);
+  });
+}
